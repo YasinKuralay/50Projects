563a59c95dd7aeed5199e1f2124d3494f8620778
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-09 10:41:47 +0100

    Project-13: Functionality added, project finished.

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value);
+
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = "";
+        }, 10);
+
+        randomSelect();
+    }
 });
 
 function createTags(input) {
@@ -21,4 +29,35 @@ function createTags(input) {
         tagEl.innerText = tag;
         tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+    let times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+        times--;
+        if (times === 0) {
+            clearInterval(interval);
+        } else {
+            setTimeout(() => {
+                unHighlightTag(randomTag);
+            }, 100);
+        }
+    }, 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight");
 }
