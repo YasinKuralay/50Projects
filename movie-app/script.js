ec6aa849d9c51cb8b8bcabf9f8d2bdda1a705f4e
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-20 04:42:11 +0100

    Project-17: JavaScript completed, project finished.

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,7 +1,8 @@
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c770da96a77f7fd10222f95ef430a824&page=1`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=c770da96a77f7fd10222f95ef430a824&query="`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=c770da96a77f7fd10222f95ef430a824&query="`; //need to elaborate, shouldn't we add an additional double quote at the end of the concatenation?
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -12,7 +13,44 @@ async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data.results);
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+                <img src="${IMG_PATH + poster_path}" alt="${title}" />
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(
+                        vote_average
+                    )}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+            `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
 }
 
 form.addEventListener('submit', (e) => {
