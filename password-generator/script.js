2e6110baf88543850706a3e1e9bc3a0864f3c6e9
Yasin S. Kuralay <yasinkuralay@gmail.com>
2021-01-06 21:17:54 +0100

    Day-31: Added click event for password generator. Iterator not running, need to fix it.

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,9 +1,55 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol,
 };
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol
+    );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+        (item) => Object.values(item)[0]
+    );
+
+    if (typesCount === 0) {
+        return '';
+    }
+    console.log('hey');
+
+    for (let i = 0; i < length; i += typesCount) {
+        console.log('heyoo');
+        typesArr.forEach((type) => {
+            const funcName = Object.keys(type)[0];
+            console.log('func name is: ', funcName);
+        });
+    }
+    console.log('end');
+}
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
