b886eea75309f7b50765b842df99f67050787b8e
Yasin S. Kuralay <yasinkuralay@gmail.com>
2021-02-16 16:55:21 +0100

    Added .gitignore and axios.

--- experimental/cron-server/index.js
+++ experimental/cron-server/index.js
@@ -1,7 +1,14 @@
+const axios = require('axios');
+
 var CronJob = require('cron').CronJob;
 var job = new CronJob(
     '*/3 * * * * *',
     function () {
+        axios
+            .get('http://daten.g-g-g.de/lagerliste/Artikelverfuegbarkeit.csv')
+            .then(({ data }) => {
+                console.log('the typeof data is: ', typeof data);
+            });
         console.log('Lets do some fetching!');
     },
     null,
